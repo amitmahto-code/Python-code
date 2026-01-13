dee11ae396ae3e144330f31fefd6d3151f0561ce
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-13 21:53:59 +0530

    learning all operators

---
+++ Chapter 2/operators.py
@@ -0,0 +1,38 @@
+# arithemetic operators
+a = 3
+b= 4
+print(a+b)
+
+#assignment operators 
+a = 5
+b += 5
+print(b)
+
+#comparsion opeators
+a = 5==5
+print(a)
+
+b = 5>8
+print(b)
+
+#logical opertors 
+a = True or False
+print(a)
+
+#truth table of 'and' 
+print("Truth table of 'And'\n")
+print("Ture and False= ", True and False)
+print("\nTure and False= ", True and True)
+print("\nTure and False= ", False and False)
+print("\nTure and False= ", False and True)
+
+#truth table of 'or' 
+print("\nTruth table of 'OR' ")
+print("\nTure or False= ", True or False)
+print("\nTure or False= ", True or True)
+print("\nTure or False= ", False or False)
+print("\nTure or False= ", False or True)
+
+#not 
+print(not(True))
+print(not(False))
