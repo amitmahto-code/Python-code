d49101dd61b384d276e83de5e0e0b8d147b6062f
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-16 19:06:20 +0530

    know about string function

---
+++ Chapter 3/string_function.py
@@ -0,0 +1,23 @@
+# # len() function 
+name = "harry"
+print(len(name))
+
+
+# string.endswitch() funtion
+name = "harry"
+print(name.endswith("rry")) 
+print(name.startswith("h"))
+print(name.capitalize())
+
+#string.count function()
+name="amit hello world"
+count = name.count("l")
+print(count)
+
+# String.find(word of indexth postion )
+names="Ramesh was nice parson"
+print(names.find("was"))
+
+#string.replace(old word,new word)
+str="hello world"
+print(str.replace("world","python"))
