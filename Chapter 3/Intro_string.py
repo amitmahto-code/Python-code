dc4500516b6dc5544699a98535eec79f4b8509de
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-16 19:04:35 +0530

    learing how to work string

---
+++ Chapter 3/Intro_string.py
@@ -0,0 +1,14 @@
+#write a string in three way
+a = 'Amit' #single quoted
+b = "Amit" #Double quoted
+c ="""Amit""" #triple quoted
+
+# indexing of string 
+name = "Amit"
+short=name[0:2]
+print(short)
+
+#Slicing with skip value
+word="amazing"
+a=word[0:4:1]
+print(a)
