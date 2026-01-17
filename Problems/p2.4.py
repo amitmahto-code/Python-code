a037d203c5dd061a0c72abb7ac99be5229b53fbc
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-17 21:14:29 +0530

    use comparison operator

---
+++ Problems/p2.4.py
@@ -0,0 +1,7 @@
+# use comparison operator to find out whether  a given variable is greater than 'b' or not 
+#take a=34, b=80
+
+a=int(input("enter number a:- "))
+b=int(input("enter number b:- "))
+# print("B is greater number ",a<b)
+print("A is greater number ",a>b)
