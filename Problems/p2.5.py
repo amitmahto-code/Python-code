75a746d619dd6e646dc6ff8a17498c14f5e349dc
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-18 21:25:14 +0530

    find the average number

---
+++ Problems/p2.5.py
@@ -0,0 +1,5 @@
+# Write a program to find the an average number of entered by the user
+a =int(input("enter number:- "))
+b =int(input("enter number:- "))
+c =int(input("enter number:- "))
+print("The average of number:- ",(a+b+c)/3)
