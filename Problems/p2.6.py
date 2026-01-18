53d94fea0d22c220eaa0f0374c987c0bcbf4a1a0
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-18 21:26:23 +0530

    input from the user and find square

---
+++ Problems/p2.6.py
@@ -0,0 +1,5 @@
+#write a program to calculate the square of a number entered by the user
+a=int(input("Enter number:- "))
+print("Square of number:- ",a*a)
+print("Square of number:- ",a**2) #second option to do power of any number 
+# print("Square of number:- ",a^2) this is not vaild  
