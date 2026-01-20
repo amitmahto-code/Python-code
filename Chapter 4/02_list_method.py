33824645ec98ee25741cc4b026102dd1253942fc
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-20 21:04:27 +0530

    method list

---
+++ Chapter 4/02_list_method.py
@@ -0,0 +1,25 @@
+"""list method also we can say function of list it change """
+List_1=[1,6,4,7,10,11]
+#sort function
+List_1.sort()#sort the list in increase order
+print(List_1)
+
+#reverse function
+List_1.reverse()
+print(List_1)
+
+# append function at the end of list add 
+List_1.append("Amit")
+print(List_1)
+
+# insert (insert (3,5) mean firstly we write index number and than write changes of that index)
+List_1.insert(3,"aamit")
+print(List_1)
+
+#pop 
+List_1.pop(3)
+print(List_1)
+
+#remove mean remove what ever exact data in list
+List_1.remove(11)
+print(List_1)
