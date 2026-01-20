c60e4df29bdff6e0e66af8246fd8a6511b1e04ce
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-20 21:07:02 +0530

    using string function replace the word

---
+++ Problems/p3.2.py
@@ -0,0 +1,7 @@
+# write a program to fill in letter template given below with name and date
+letter="""Dear name,
+you are selected
+date"""
+print(letter.replace("name","Amit").replace("date","01 feb 2025"))
+
+
