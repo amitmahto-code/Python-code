aa4d83ccb6b0864e791dec7dd570a563b66c4c9d
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-20 21:03:17 +0530

    What is list

---
+++ Chapter 4/01_list.py
@@ -0,0 +1,9 @@
+# python lists are continers to store a set of values of any data type
+friend=["Amit","Rahul",1,False]
+print(friend)
+#unlike string list is mutable
+
+friend[0]="Kunal" 
+friend[1]="Amit"
+print(friend[0])#using indexing for print 
+print(friend[1:4])#using indexing print list start to end print
