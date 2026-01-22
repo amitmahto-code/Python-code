48407ceb87978d8af8e5bb4059a3022ac1b92ba6
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-22 18:41:14 +0530

    concept of tuple

---
+++ Chapter 4/tuple.py
@@ -0,0 +1,6 @@
+# tuple is immutable data type in python 
+a = (1,"amit",5,7,False)# this is more one elemet 
+b = ()#thid id empty tuple
+c = (1,)#tuple with single element
+print(type(a),type(b),type(c))
+print(a,b,c)
