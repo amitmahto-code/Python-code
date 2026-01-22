3eac94c3060b00f56b295849d2d88034e455dd72
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-22 18:41:53 +0530

    some tuple method

---
+++ Chapter 4/tuple_method.py
@@ -0,0 +1,13 @@
+# tuples method 
+
+#count method
+a = (1,"amit",5,6,258)
+no = a.count(5) # this method retuen no.of.time repeat in tuple
+print(no)
+
+#indexed method
+pos = a.index("amit")
+print(pos)
+
+#length of tuple
+print(len(a))
