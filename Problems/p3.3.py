e420d97a334eea6f695e90203b9a288ff23dcb64
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-22 18:44:07 +0530

    detect double space

---
+++ Problems/p3.3.py
@@ -0,0 +1,5 @@
+# write a program to detect double space in a string
+name="Good morning  Mr  amit  "
+print(name.find("  "))
+# replace the double space in single space
+print(name.replace("  "," "))
