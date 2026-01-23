a78545c91f1e7c50cf6491b6b80837c07bc5a5ca
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-23 22:06:29 +0530

    input seven fruit list

---
+++ Problems/p4.1.py
@@ -0,0 +1,11 @@
+# write a program to store seven fruits in a list entered by the user.
+l1 =["","","","","","",""]
+l1[0]=input("enter 1 name :")
+l1[1]=input("enter 2 name :")
+l1[2]=input("enter 3 name :")
+l1[3]=input("enter 4 name :")
+l1[4]=input("enter 5 name :")
+l1[5]=input("enter 6 name :")
+l1[6]=input("enter 7 name :")
+
+print(l1)
