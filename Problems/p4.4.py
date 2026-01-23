7c3c0fd5ccfdf2790c23c53891e95148d5114e37
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-23 22:08:59 +0530

    display the sum of number in list

---
+++ Problems/p4.4.py
@@ -0,0 +1,8 @@
+# # write a program to sum a list with 4 number
+# num = [3,4,5,6,7]
+# sum=num[0]+num[1]+num[2]+num[3]+num[4]
+# print(sum)
+# either we can use function sum
+number=[5,6,78,100,300]
+
+print(sum(number))# sum is function 
