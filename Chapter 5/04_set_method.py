71db22e838c8407daaad760d20d926dcc5b4a197
amitmahto-code <mahtoamit6678@gmail.com>
2026-02-25 23:29:43 +0530

    set methods

---
+++ Chapter 5/04_set_method.py
@@ -0,0 +1,19 @@
+#add method
+s ={1,56,7,84,23,55,55}
+s.add("owner")
+print(s)
+# .remove 
+s.remove(7)
+print(s)
+
+#intersection and union 
+s1 = {1,4,3,55}
+s2 = {1,4,7,5}
+print(s1.union(s2))
+print(s1.intersection(s2))
+
+#clear set .clear
+s3 = {3,4,56,9}
+s3.clear()
+print(s3)
+
