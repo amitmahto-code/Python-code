686c6495bb7181b139c14c7e2d4e02d44c3e5f8a
amitmahto-code <mahtoamit6678@gmail.com>
2026-04-08 17:07:42 +0530

    condition statement

---
+++ chapter 6/01_condition.py
@@ -0,0 +1,9 @@
+a=int(input("enter your age= "))
+if(a>=18):
+    print("your are eligible for consent")
+elif(a<0):
+    print("Your are enter negative number")
+else:
+    print("Your are not eligible for consent")
+print("program end")
+    
