daea8beb189e60356dedb55ff88ab25cd20bdcc1
amitmahto-code <mahtoamit6678@gmail.com>
2026-04-10 16:48:33 +0530

    perameter and argument

---
+++ chapter 8/agrument.py
@@ -0,0 +1,6 @@
+def greet(name):  
+    gr = "Thank you " + name
+    print("thank you" + name)
+    return gr
+a = greet(" Amit")
+print(a)
---
+++ chapter 8/argument.py
@@ -0,0 +1,6 @@
+def greet(name):  #here inside the greet("name") name is perameter
+    gr = "Thank you " + name
+    print("thank you" + name)
+    return gr
+a = greet(" Amit") #here inside the greet("Amit") amit is argument
+print(a)
