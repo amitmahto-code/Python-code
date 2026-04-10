c0cec1e52425dd6e1ef1cb46dd5f82d09867d17a
amitmahto-code <mahtoamit6678@gmail.com>
2026-04-10 16:43:15 +0530

    know about function part

---
+++ chapter 8/function.py
@@ -0,0 +1,10 @@
+#create a function 
+
+#function defination
+def sum():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Sum of two number:= ",a+b)
+
+#thi is function call
+sum()
