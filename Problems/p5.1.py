f904a70db47663cfb1089fcd23c995821e61812e
Mahto Amit <mahtoamit6678@gmail.com>
2026-05-09 15:23:48 +0530

    dictionary of hindi words with values as their english translation.

---
+++ Problems/p5.1.py
@@ -0,0 +1,12 @@
+#write a program to create a dictionary of hindi words with values as their english translation.
+#Provide user with an option to look up
+
+word = {
+    "May":"I",
+    "Tum":"You",
+    "likhna":"write"
+
+}
+words=input("Enter the meaning you want in english meaning: ")
+print(word[words])
+
