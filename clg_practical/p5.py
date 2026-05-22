9e8875588dee2f9196c92ff600d7c1f7618c6b65
Mahto amit <mahtoamit6678@gmail.com>
2026-05-22 12:54:52 +0530

    checkt whether a number is positive, negative or zero

---
+++ clg_practical/p5.py
@@ -0,0 +1,9 @@
+# write a python program to find whether a number is positive , negative or zero
+n = int(input("Enter number: "))
+
+if n > 0 :
+    print("Positive number ")
+elif n < 0 :
+    print("Negative number ")
+else:
+    print("Zero")
