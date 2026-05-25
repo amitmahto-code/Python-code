74b1d4efa73de4cc6c494c342021ddd96e5b8bbd
Mahto amit <mahtoamit6678@gmail.com>
2026-05-25 18:07:15 +0530

    count total number of times a Specified value appears in given string.

---
+++ clg_practical/p12.py
@@ -0,0 +1,7 @@
+#Write a python program to count total number of times a Specified value appears in given string.
+
+text = "Ram is good boy"
+search =input("enter specified value: ")
+
+n=text.count(search)
+print("Number of time appears in string ",n)
