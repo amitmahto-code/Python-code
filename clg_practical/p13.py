8fc624032ce438184262107ceea48997451aaddf
Mahto amit <mahtoamit6678@gmail.com>
2026-05-25 18:07:54 +0530

    add and remove element from the list

---
+++ clg_practical/p13.py
@@ -0,0 +1,6 @@
+#Write a python program to add and remove element from the list
+l1=["Amit",1,25000]
+l1.append(8)
+print("After adding 8 in list",l1)
+l1.remove(1)
+print("After removing 1 in list",l1)
